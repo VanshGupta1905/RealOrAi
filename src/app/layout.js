3d29d1ba0cf856d4d4af03fa569e8d3f9064eeab
honeybansal2968 <honeybansal2968@gmail.com>
2024-03-06 09:46:10 +0000

    added logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/dive.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Cursor />
         <div id="main">
